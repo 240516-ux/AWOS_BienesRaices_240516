da89e45515122dfa2996ee7ea6348904ce2ee2da
alancruzbaltazar <240516@utxicotepec.edu.mx>
2026-03-26 11:10:05 -0600

    Seccion 7 terminada

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express'
+console.log(" RUTAS DE USUARIO CARGADAS");
 import { 
     formularioLogin, 
     formularioRegistro, 
@@ -10,12 +11,14 @@ import {
 const router = express.Router()
 
 // GET
-router.get('/login', formularioLogin)
+router.get('/login', (req, res) => {
+    res.send(" LOGIN ROUTE FUNCIONA");
+});
 router.get('/registro', formularioRegistro)
 router.get('/recuperarPassword', formularioRecuperacion)
-router.get("/confirma/:token", paginaConfirmacion)
+router.get('/confirma/:token', paginaConfirmacion)
 
-//POST
-router.post("/registro", registrarUsuario)
+// POST
+router.post('/registro', registrarUsuario)
 
 export default router
